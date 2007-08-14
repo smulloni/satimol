d0c91a42345572c1a9fb9971a8b7a9f11f41f1df
jsmullyan <jsmullyan@3680bf0b-9a37-0410-ac13-9387e4187c76>
2007-08-14 21:38:27 +0000

    updated

--- setup.py
+++ setup.py
@@ -7,10 +7,11 @@ description="caching and templating facilities from the SkunkWeb project"
 
 long_description="""
 
-The skunk libraries include a caching system for memoizing callables,
-with in-memory, disk, or memcached backends, and a standalone
-implementation of SkunkWeb's STML (Skunk Template Markup Language), a
-powerful componentized templating language.
+SaTiMoL is the latest incarnation of the SkunkWeb libraries, which
+include a caching system for memoizing callables, with in-memory,
+disk, or memcached backends, and a standalone implementation of
+SkunkWeb's STML (Skunk Template Markup Language), a powerful
+componentized templating language.
 
 
 """
@@ -23,7 +24,7 @@ setup(author="Jacob Smullyan",
       long_description=long_description,
       license="BSD/GPL",
       platforms='OS Independent',
-      name="skunk.stml",
+      name="satimol",
       url="http://code.google.com/p/satimol/",
       classifiers=["Development Status :: 4 - Beta",
                    "Intended Audience :: Developers",
