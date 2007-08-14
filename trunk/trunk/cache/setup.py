378ade6ff9647f617ace2e308c2c554c26f04929
jsmullyan <jsmullyan@3680bf0b-9a37-0410-ac13-9387e4187c76>
2007-08-14 19:19:34 +0000

    more svn:ignore stuff

--- trunk/trunk/cache/setup.py
+++ trunk/trunk/cache/setup.py
@@ -3,15 +3,30 @@ ez_setup.use_setuptools()
 
 from setuptools import setup
 
-description="skunk.cache is a robust memoization facility."
+description="a memoization library"
+
+long_description="""
+
+skunk.cache is a simple but robust memoization facility for most
+callables with in-memory, on-disk, and memcached backends.  It is the
+descendent of the SkunkWeb cache system.  
+
+"""
+
+import sys
+sys.path.insert(0, 'src')
+import skunk.cache
+version=skunk.cache.__version__
 
 setup(author="Jacob Smullyan",
       author_email='smulloni@smullyan.org',
       description=description,
+      long_description=long_description,
       license="BSD/GPL",
+      platforms='OS Independent',
       name="skunk.cache",
       url="http://code.google.com/p/satimol/",
-      version="4.0.1",
+      version=version,
       zip_safe=True,
       keywords="cache skunk skunkweb",
       namespace_packages=['skunk'],
