3ce417ad5bb8ab3184e9cea3b3a26a2a3c39b427
jsmullyan <jsmullyan@3680bf0b-9a37-0410-ac13-9387e4187c76>
2007-08-14 17:51:23 +0000

    fix to setup.py, adding README

---
+++ cache/README.txt
@@ -0,0 +1,36 @@
+===========
+skunk.cache
+===========
+
+The ``skunk.cache`` package is a simple memoization facility for
+callables with in-memory, on-disk, and memcached backends.
+
+An Example
+==========
+
+Typical usage::
+
+  >>> from skunk.cache import *
+  >>> mycache=DiskCache('/tmp/mycache')
+  >>> cache=CacheDecorator(mycache, defaultPolicy=YES)
+  >>> @cache(expiration="5m")
+  ... def foo(x, y):
+  ...    print "actually calculating ...."
+  ...    return x + y
+  >>> foo(5, 5)
+  actually calculating ....
+  10
+  >>> foo(5, 5)
+  10
+  
+
+Cache Expiration
+================
+
+To be explained.
+
+
+
+
+
+
--- cache/setup.py
+++ cache/setup.py
@@ -3,17 +3,17 @@ ez_setup.use_setuptools()
 
 from setuptools import setup
 
-description="Skunk Cache is a robust memoization facility."
+description="skunk.cache is a robust memoization facility."
 
 setup(author="Jacob Smullyan",
       author_email='smulloni@smullyan.org',
       description=description,
       license="BSD/GPL",
-      name="Skunk Cache",
-      url="http://skunkweb.org/",
+      name="skunk.cache",
+      url="http://code.google.com/p/satimol/",
       version="4.0.1",
       zip_safe=True,
       keywords="cache skunk skunkweb",
       namespace_packages=['skunk'],
-      packages=['skunk', 'skunk.cache']
+      packages=['skunk', 'skunk.cache'],
       package_dir={'' : 'src'})
