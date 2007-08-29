35d587eb9e8507682bcc311d2c7a433227a31bb0
jsmullyan <jsmullyan@3680bf0b-9a37-0410-ac13-9387e4187c76>
2007-08-29 17:52:53 +0000

    adding SKUNK_VERSION

--- src/skunk/web/__init__.py
+++ src/skunk/web/__init__.py
@@ -1,10 +1,14 @@
+SKUNK_VERSION="4.0a0"
+
+
 from skunk.config import Configuration
 
-
 Configuration.setDefaults(
+    serverIdentification='Skunkweb %s' % SKUNK_VERSION,
     defaultCharset='utf-8',
     defaultContentType='text/html'
     )
 
 del Configuration
+
 
