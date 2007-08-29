f1dbce57df51d5f65fe27c2f0f927908d6cda16b
jsmullyan <jsmullyan@3680bf0b-9a37-0410-ac13-9387e4187c76>
2007-08-29 22:33:52 +0000

    gradual progress

--- src/skunk/web/controller.py
+++ src/skunk/web/controller.py
@@ -1,89 +1,116 @@
+import httplib
 import logging
 import sys
-import traceback
+import types
+
+import webob    
 
 from skunk.config import Configuration
 from skunk.util.importutil import import_from_string
 from skunk.web.context import Context
+from skunk.web.util import get_http_exception
 
 log=logging.getLogger(__name__)
 
 Configuration.setDefaults(controllers=[])
 
-def handle_error(message,
-                 method,
-                 controller,
-                 action,
-                 args,
-                 kwargs):
-    res=Configuration.errorHandlers[500]()
-    # do something here
-    return res
+def expose(**kwargs):
+    def wrapper(func):
+        func.exposed=True
+        for k,v in kwargs.iteritems():
+            setattr(func, k, v)
+        return func
+    return wrapper
+    
+def _interpret_response(res):
+    """
+    coerces the response into a webob.Response object.
 
+    Types accepted:
+
+    - a webob.Response
+    - None, in which case Context.response is returned if it has a body,
+      and otherwise, a 404
+    - a string, which is set to the body of Context.response and the latter
+      is returned
+    - a unicode string (similar)
+    - a list, tuple or generator -- becomes Context.response.app_iter
+
+    Support for return a dict should be added, and invoking the templating
+    system directly.
+    
+    """
+    if isinstance(res, webob.Response):
+        return res
+    ctxt_res=Context.response
+    if res is None:
+        if ctxt_res.app_iter or ctxt_res.body:
+            # something has been done to it
+            return ctxt_res
+        else:
+            return get_http_exception(httplib.NOT_FOUND)
+    elif isinstance(res, str):
+        ctxt_res.body=res
+        return ctxt_res
+    elif isinstance(res, unicode):
+        ctxt_res.unicode_body=res
+        return ctxt_res
+    elif isinstance(res, (list,tuple,types.GeneratorType)):
+        ctxt_res.app_iter=res
+        return ctxt_res
+    elif isinstance(res, int):
+        try:
+            return get_http_exception(res)
+        except KeyError:
+            pass
+    # last resort
+    try:
+        ctxt_res.body=str(res)
+    except:
+        pass
+    return get_http_exception(httplib.NOT_FOUND)    
 
 def dispatch_from_environ(environ, next_app=None):
+    """
+    dispatch according to the wsgi routing specification
+    """
     try:
         tmp=environ['wsgiorg.routing_args']
     except KeyError:
         return None
     else:
         margs, mkwargs=tmp
-    return dispatch(next_app=next_app, *margs, **mkwargs)
+    return dispatch(*margs, **mkwargs)
 
-def dispatch(next_app=None, *args, **kwargs):
+def dispatch(*args, **kwargs):
     controller=kwargs.get('controller')
     if not controller:
-        return 
-    action=kwargs.get('action', 'index')
+        return
+
     if isinstance(controller, basestring):
         try:
             controller=import_from_string(controller)
         except ImportError:
-            handle_error("dispatcher couldn't import name '%s'" % controller,
-                         None,
-                         controller,
-                         action,
-                         args,
-                         kwargs)
-            
-    
-    meth=getattr(controller, action, None)
-    if meth:
-        if not getattr(meth, 'exposed', False):
-            log.info("request for method that isn't exposed, not honoring")
-            return
-        try:
-            res=meth(*args, **kwargs)
-        except webob.exc.HTTPException, e:
-            res=e
-        except:
-            exc=sys.exc_info()[
-            return handle_error("error in controller method",
-                                meth,
-                                controller,
-                                action,
-                                args,
-                                kwargs)
+            return get_http_exception(httplib.SERVER_ERROR,
+                                      comment="couldn't import controller %s" % controller)
         
-        if isinstance(res, webob.Response):
-            return res
-        if isinstance(res, str):
-            gres=Context.response
-            gres.body=res
-            return gres
-        elif isinstance(res, unicode):
-            gres=Context.response
-            gres.unicode_body=res
-            return gres
-        elif isinstance(res, (list,tuple)):
-            gres=Context.response
-            gres.app_iter=res
-            return gres
-        elif isinstance(res, int):
-            try:
-                return Configuration.errorHandlers[res]()
-            except KeyError:
-                return str(res)
+    action=kwargs.get('action', 'index')    
+    meth=getattr(controller, action, None)
+    if not meth:
+        return 
+
+    if not getattr(meth, 'exposed', False):
+        log.info("request for method that isn't exposed, not honoring")
+        return
+    
+    try:
+        res=meth(*args, **kwargs)
+    except webob.exc.HTTPException, e:
+        res=e
+
+    return _interpret_response(res)
+
+
 
 
     
