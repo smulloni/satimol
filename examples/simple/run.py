b367fb54991db162df80911d0961669bb2cabc09
jsmullyan <jsmullyan@3680bf0b-9a37-0410-ac13-9387e4187c76>
2007-09-04 15:58:06 +0000

    consolidating into a single file

--- examples/simple/run.py
+++ examples/simple/run.py
@@ -1,3 +1,10 @@
+"""
+This is a simple example of setting up a skunkweb instance manually,
+loading configuration from keywords, rather than from a file,
+composing a WSGI application, and serving it (in this case with
+cherrypy's wsgi server, but any one can be used).
+"""
+
 import logging
 import os
 
@@ -9,19 +16,49 @@ from skunk.web.controller import expose, ControllerServer
 from skunk.web.fileserver  import DispatchingFileServer
 from skunk.web.routing import RoutingMiddleware
 
-from someControllers import *
+
+class SimpleController(object):
+
+    @expose()
+    def index(self):
+        return """
+        <html>
+        <head>
+        <titleHome Page</title>
+        </head>
+        <body>
+        <h1>Welcome to Satimol</h1>
+        </body>
+        </html>
+        """
+
+    @expose(content_type='text/plain')
+    def robots(self, color):
+        return "Your robots are about to turn " + color
+
+    
+    @expose()
+    def wsgi(self):
+        def an_app(environ, start_response):
+            start_response('200 OK', [('Content-Type' , 'text/plain')])
+            return ['Hello from a WSGI application']
+        return an_app
 
 logging.basicConfig(level=logging.DEBUG)
 
-Configuration.load_kw(componentRoot=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'files'),
-                      routes=[
-    (('robots', '/robots/:color'), {'controller' : 'home',
-                                    'action' : 'robots'}),
-    (('wsgi','/wsgi'), {'controller' : 'home', 'action' : 'wsgi'}),
-    (('hello', '/hello'), {'controller' : 'home'}),
+comproot=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'files')
+
+Configuration.load_kw(
+    componentRoot=comproot,
+    routes=[
+    (('robots', '/robots/:color'),
+     {'controller' : 'simple', 'action' : 'robots'}),
+    (('wsgi','/wsgi'),
+     {'controller' : 'simple', 'action' : 'wsgi'}),
+    (('hello', '/hello'), {'controller' : 'simple'}),
     ],
-                      controllers={'home' : HomeController()},
-                      MvcOn=True)
+    controllers={'simple' : SimpleController()},
+    MvcOn=True)
 
 app=ContextMiddleware(RoutingMiddleware(ControllerServer(DispatchingFileServer())))
 
@@ -30,6 +67,8 @@ server=CherryPyWSGIServer(('localhost', 7777), app, server_name='localhost')
 
 if __name__=='__main__':
     try:
+        print "starting server"        
         server.start()
     except KeyboardInterrupt:
         server.stop()
+        print "server stopped"
