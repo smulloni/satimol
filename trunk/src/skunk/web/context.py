459bf5695eada12218a341cc38fbd81f916818b4
jsmullyan <jsmullyan@3680bf0b-9a37-0410-ac13-9387e4187c76>
2007-09-09 19:52:55 +0000

    adding PreConfigHook

--- trunk/src/skunk/web/context.py
+++ trunk/src/skunk/web/context.py
@@ -5,10 +5,24 @@ import webob
 from skunk.config import Configuration
 from skunk.util.hooks import Hook
 
+# A container for Request, Response, and possibly other things
 Context=threading.local()
 
+# A hook for actions before the configuration object is scoped.
+# Each function will be passed the environment later passed to
+# Configuration.scope(); this is the wsgi environ, with some additional
+# keys:
+#    url
+#    path
+PreConfigHook=Hook()
+
+# a hook for actions after the configuration is scoped and the Context
+# object has been initialized with Request and Response objects.  Each
+# function is passed the Context object and the wsgi environ.
 InitContextHook=Hook()
 
+# a hook at the end of request processing for any cleanup operations.  Each
+# function is passed the Context object and the wsgi environ.
 CleanupContextHook=Hook()
 
 class ContextMiddleware(object):
@@ -27,6 +41,9 @@ class ContextMiddleware(object):
         # add some useful calculated info to env 
         env['url']=req.url
         env['path']=req.path
+
+        PreConfigHook(env)
+        
         Configuration.scope(env)
         
         Context.request=req
