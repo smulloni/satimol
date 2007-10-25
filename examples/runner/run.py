cf2b90f219bc825a12e4c0e5df09a4e30e15928e
jsmullyan <jsmullyan@3680bf0b-9a37-0410-ac13-9387e4187c76>
2007-10-25 15:12:32 +0000

    adding example that uses skunk.web.runner

---
+++ examples/runner/run.py
@@ -0,0 +1,29 @@
+import logging
+import os
+import sys
+
+from skunk.config import Configuration
+from skunk.web import ContextMiddleware, RoutingMiddleware, ControllerServer, expose
+from skunk.web.runner import run
+
+logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+
+@expose(content_type='text/plain')
+def helloworld():
+    return "Hello World"
+
+
+Configuration.load_kw(
+    daemonize=True,
+    useThreads=True,
+    serverProtocol='fcgi',
+    pidfile=os.path.abspath('simplerunner.pid'),
+    controllers=dict(hello=__name__),
+    routes=[(('index', '/'),
+             {'controller' : 'hello',
+              'action' : 'helloworld'})],
+    MvcOn=True,
+    bindAddress='TCP:0.0.0.0:7777')
+
+app=ContextMiddleware(RoutingMiddleware(ControllerServer()))
+run(app)
