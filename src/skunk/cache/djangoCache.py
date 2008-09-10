4163d4286859d3ee21c77a55336d808ef38503c2
jsmullyan <jsmullyan@3680bf0b-9a37-0410-ac13-9387e4187c76>
2008-09-10 19:56:25 +0000

    adding wrapper around the django cache

---
+++ src/skunk/cache/djangoCache.py
@@ -0,0 +1,25 @@
+import time
+import warnings
+
+from django.core.cache import cache as djCache
+
+from skunk.cache.base import Cache
+
+class DjangoCache(Cache):
+
+    def _retrieve(self, canonicalName, cacheKey):
+        return djCache.get((canonicalName, cacheKey), NULL)
+
+    def _store(self, entry, canonicalName, cacheKey):
+        now=time.time()
+        entry.stored=now
+        expiration=max(now, entry.expiration)-now
+        djCache.set((canonicalName, cacheKey), entry, expiration)
+
+    def invalidate(self, canonicalName):
+
+        warnings.warn("cache invalidation is not currently supported by the django backend",
+                      NotImplementedWarning)
+
+        
+
