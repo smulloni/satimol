1edf6da86acba8ffaf5664dc91cba050129eb388
jsmullyan <jsmullyan@3680bf0b-9a37-0410-ac13-9387e4187c76>
2008-10-04 02:43:48 +0000

    possible fix for django memcache

--- src/skunk/cache/djangoCache.py
+++ src/skunk/cache/djangoCache.py
@@ -7,14 +7,19 @@ from skunk.cache.base import Cache
 
 class DjangoCache(Cache):
 
+    def _get_key(self, canonicalName, cacheKey):
+        t=(canonicalName, cacheKey)
+        return "%s---%s" % t
+
+
     def _retrieve(self, canonicalName, cacheKey):
-        return djCache.get((canonicalName, cacheKey))
+        return djCache.get(self._get_key(canonicalName, cacheKey))
 
     def _store(self, entry, canonicalName, cacheKey):
         now=time.time()
         entry.stored=now
         expiration=max(now, entry.expiration)-now
-        djCache.set((canonicalName, cacheKey), entry, expiration)
+        djCache.set(self._get_key(canonicalName, cacheKey), entry, expiration)
 
     def invalidate(self, canonicalName):
 
